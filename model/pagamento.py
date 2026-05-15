d2b389e846821bdbc812bb0171e65bbfb6edf334
Bruno Mafei E. D <brunomafei@gmail.com>
2026-05-15 18:18:02 +0000

    criação das classes

---
+++ model/pagamento.py
@@ -0,0 +1,5 @@
+# classe abstrata
+# PagamentoDinheiro
+# PagamentoPix
+# PagamentoCartao
+# Necessárias porque o sistema exige dados específicos para cada modalidade (como CPF no PIX ou Bandeira no Cartão).
---
+++ model/pessoa.py
@@ -0,0 +1,4 @@
+# classe abstrata
+# Pessoa (Abstrata): Essencial para aplicar o conceito de classe abstrata e herança. Contém nome, celular e cpf.  
+# Paciente: Herda de Pessoa. Necessária para a regra de negócio da idade (+18 anos).
+# Profissional: Herda de Pessoa. Contém especialidade e registro.  
