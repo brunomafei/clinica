c6a5bbc8631666c450e1dd9253789cca1adcb457
Bruno Mafei E. D <brunomafei@gmail.com>
2026-05-18 15:04:55 +0000

    pessoa atualizada

--- model/paciente.py
+++
@@ -1,1 +0,0 @@
-#herda pessoa
--- model/pessoa.py
+++ model/pessoa.py
@@ -1,4 +1,81 @@
-# classe abstrata
-# Pessoa (Abstrata): Essencial para aplicar o conceito de classe abstrata e herança. Contém nome, celular e cpf.  
-# Paciente: Herda de Pessoa. Necessária para a regra de negócio da idade (+18 anos).
-# Profissional: Herda de Pessoa. Contém especialidade e registro.  
+from abc import ABC, abstractmethod
+
+class Pessoa(ABC):
+    def __init__(self, nome: str, celular: str, cpf: str):
+        self.__nome = nome
+        self.__celular = celular
+        self.__cpf = cpf
+
+ #property e setters--------------------------------------------------------------------------------------
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def celular(self):
+        return self.__celular
+
+    @celular.setter
+    def celular(self, celular):
+        self.__celular = celular
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self.__cpf = cpf
+
+#-----------------------------------------------------------------------------------------------------------
+
+class Paciente(Pessoa):
+    def __init__(self, nome: str, celular: str, cpf: str, idade: int):
+        super().__init__(nome, celular, cpf)
+        self.__idade = idade
+
+#getter e setter, confirmação de idade do paciente
+    @property
+    def idade(self):
+        return self.__idade
+
+    @idade.setter
+    def idade(self, idade):
+        self.__idade = idade
+
+    def idade_paciente(self):
+
+
+
+#------------------------------------------------------------------------------------------------------------
+
+class Profissional(Pessoa):
+    def __init__(self, nome: str, celular: str, cpf: str, especialidade: str, registro: str):
+        super().__init__(nome, celular, cpf)
+        self.__especialidade = especialidade
+        self.__registro = registro
+
+    # Getters e Setters específicos do profissional
+
+    @property
+    def especialidade(self):
+        return self.__especialidade
+    
+    @especialidade.setter
+    def especialidade(self, especialidade):
+        self.__especialidade = especialidade
+
+    @property
+    def registro(self):
+        return self.__registro
+    
+    @registro.setter
+    def registro(self, registro):
+        self.__registro = registro
+
+#------------------------------------------------------------------------------------------------------------
