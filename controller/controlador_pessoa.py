2bc2bacd07c1a8f266af38d500109218e4fcf771
Bruno Mafei E. D <brunomafei@gmail.com>
2026-06-01 17:18:34 +0000

    ATUALIZADO E FINALIZADO PESSOA

--- controller/controlador_pessoa.py
+++ controller/controlador_pessoa.py
@@ -1,48 +1,75 @@
 from model.pessoa import Pessoa
+from model.pessoa import Paciente    
+from model.pessoa import Profissional
 from exceptions.elemento_nao_existe_exception import ElementoNaoExisteException
 from exceptions.elemento_repetido_exception import ElementoRepetidoException
 
-
 class ControladorPessoa:
     def __init__(self):
+        # lista de pacientes e profissionais
         self.__pessoas = []
 
-# cadastra uma nova pessoa e adiciona na lista de pessoas
-    def cadastrar_pessoa(self, nome, idade, celular, cpf):
-        # Verifica se já existe alguém com esse CPF no sistema
+# CADASTRO --------------------------------------------------------------------------------------
+    # Cadastra Paciente
+    def cadastrar_paciente(self, nome, idade, celular, cpf):
         if self.buscar_pessoa_por_cpf(cpf) is not None:
-            raise ElementoRepetidoException(f"Já existe uma pessoa cadastrada com esse CPF")
-        # Se não tiver ninguém com esse CPF, cria a pessoa normalmente e adiciona na lista
-        pessoa = Pessoa(nome, idade, celular, cpf)
-        self.__pessoas.append(pessoa)
-        return pessoa
-    
-# lista todas as pessoas cadastradas
+            raise ElementoRepetidoException("Já existe uma pessoa cadastrada com esse CPF.")
+        
+        # Cria um PACIENTE e adiciona na lista
+        paciente = Paciente(nome, idade, celular, cpf)
+        self.__pessoas.append(paciente)
+        return paciente
+
+    # Cadastra Profissional
+    def cadastrar_profissional(self, nome, celular, cpf, especialidade, registro):
+        if self.buscar_pessoa_por_cpf(cpf) is not None:
+            raise ElementoRepetidoException("Já existe uma pessoa cadastrada com esse CPF.")
+        
+        # Cria um PROFISSIONAL e adiciona na lista
+        profissional = Profissional(nome, celular, cpf, especialidade, registro)
+        self.__pessoas.append(profissional)
+        return profissional
+
+# LISTAGEM ------------------------------------------------------------------------------------------------------------------
     def listar_pessoas(self):
         return self.__pessoas
-    
-# busca pessoa pelo cpf, se não encontrar retorna None
+
+# BUSCA --------------------------------------------------------------------------------------------------------------------- 
     def buscar_pessoa_por_cpf(self, cpf):
         for pessoa in self.__pessoas:
-            if pessoa.cpf == cpf:
+            
+            if pessoa.cpf == cpf: 
                 return pessoa
         return None
     
-    def alterar_pessoa(self, pessoa, novo_nome, nova_idade, novo_celular, novo_cpf):
-        # Verifica se o objeto pessoa existe na lista de pessoas.
+# ATUALIZAÇÃO ------------------------------------------------------------------------------------------------------------------    
+    def alterar_pessoa(self, pessoa, novo_nome, novo_celular, novo_cpf, nova_idade=None, nova_especialidade=None, novo_registro=None):
         if pessoa not in self.__pessoas:
-            # Lança o erro e interrompe a função
             raise ElementoNaoExisteException("Pessoa não encontrada no sistema.")
 
-        # Atualiza os dados da pessoa já cadastrada
-        pessoa.__nome = novo_nome
-        pessoa.__idade = nova_idade
-        pessoa.__celular = novo_celular
-        pessoa.__cpf = novo_cpf
+        # identifica se o cpf e diferente do atual e se já existe outra pessoa com o novo cpf
+        if pessoa.cpf != novo_cpf and self.buscar_pessoa_por_cpf(novo_cpf) is not None:
+            raise ElementoRepetidoException("O novo CPF informado já pertence a outra pessoa.")
+        
+        # Atualiza os dados comuns
+        pessoa.nome = novo_nome
+        pessoa.celular = novo_celular
+        pessoa.cpf = novo_cpf
+
+        # Atualiza os dados específicos com base no tipo da pessoa
+        if isinstance(pessoa, Paciente):
+            if nova_idade is not None:
+                pessoa.idade = nova_idade
+                
+        elif isinstance(pessoa, Profissional):
+            if nova_especialidade is not None:
+                pessoa.especialidade = nova_especialidade
+            if novo_registro is not None:
+                pessoa.registro = novo_registro
+                
         return pessoa
 
-#remove uma pessoa da lista de pessoas
-
+# REMOÇÃO ------------------------------------------------------------------------------------------------------------------
     def remover_pessoa(self, pessoa):
         if pessoa in self.__pessoas:
             self.__pessoas.remove(pessoa)
